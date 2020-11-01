382875da5c8a99342c8e36560ed611b9652f86fb
Thomas Burnett-Taylor <tmfksoft@gmail.com>
2020-11-01 02:10:17 +0000

    chore: Added default config for Dokku
    Added process env vars for Heroku/Dokku using PORT for the default PORT for HTTP.
    
    OWM_KEY for OpenWeatherMaps Key
    GMAP_KEY for Google Maps Key

---
+++ config.js
@@ -0,0 +1,28 @@
+config = {
+    "servers": {
+        "hapi": {
+            "host": "0.0.0.0",
+            "port": process.env.PORT
+        },
+        "redis": {
+            "host": "127.0.0.1",
+            "port": 6379
+        }
+	},
+	"general": {
+		"ratelimit": 50,
+		"trustedProxies": [
+			"127.0.0.1"
+		]
+	},
+	"logging":{
+		"format":"[%date%] %level% %text%",
+		"date-format":"h:MM TT mmmm d, yyyy"
+	},
+	"dev": true,
+	"keys":{
+		"weather": process.env.OWM_KEY,
+    "google_maps": process.env.GMAP_KEY,
+	}
+};
+module.exports = config;
